1c975985f486c38c99edbbc7a9a314a6c029bd13
ihkaru <221810336@stis.ac.id>
2023-01-12 16:03:31 +0700

    add artisan

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -10,4 +10,38 @@ use Illuminate\Routing\Controller as BaseController;
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    public function responsejson($code,$data,$message = ''){
+        $status = ($code>=200 && $code<300)?true:false;
+        if($message == ''){
+            return response()->json([
+                "status"=>$status,
+                "code"=>$code,
+                "data"=>$data
+            ],$code);
+        }else{
+            return response()->json([
+                "status"=>$status,
+                "code"=>$code,
+                "data"=>$data,
+                "message"=>$message
+            ],$code);
+        }
+    }
+
+    public function responseabort($code,$message){
+        return $this->responsejson($code,["message"=>$message]);
+    }
+
+    public function emptyHandler(String $key){
+        if(request($key)){
+            return request($key);
+        }else{
+            if(request()->exists($key)){
+                return "";
+            }else {
+                return null;
+            }
+        }
+    }
 }
